2dd9a067685c3906284ab9d300ea7a360d0ffba7
Paola Becerra <paonata_marquez@outlook.com>
2022-03-17 20:05:13 -0500

    change the project to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import * as React from "react";
 import "./App.css";
+// TODO: fix type
+// @ts-ignore
+import Listing from "./components/organisms/Listing/index.tsx";
 
-function App() {
-  return <div className="App">Cartelera</div>;
-}
+const App = (): JSX.Element => {
+  return (
+    <div className="App">
+      <Listing />
+    </div>
+  );
+};
 
 export default App;
