1ff7d8af8ad12bb4b8398d75c9533e1155e9a49b
Paola Becerra <paonata_marquez@outlook.com>
2022-03-17 20:05:27 -0500

    add listing movies structure component

---
+++ src/components/organisms/Listing/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { ListingMoviesWrapper } from "./styles";
+
+const ListingComponent = ({}): JSX.Element => {
+  return <div>lista de peliculas</div>;
+};
+
+export default ListingComponent;
