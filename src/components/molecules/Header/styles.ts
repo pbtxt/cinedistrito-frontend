17e6c45fe53bcb0626d83208d5b366f8609b6876
Paola Becerra <paonata_marquez@outlook.com>
2022-03-19 11:57:11 -0500

    add header structure and styles

---
+++ src/components/molecules/Header/styles.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+// @ts-ignore
+export const HeaderContainer = styled.div`
+  width: 100%;
+  height: 70px;
+  background-color: #000;
+  display: flex;
+  align-items: center;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+// @ts-ignore
+export const HeaderItemContainer = styled.div`
+  
+`;
+
+// @ts-ignore
+export const HeaderItemsContainer = styled.div`
+  width: 400px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  a {
+    color: white;
+  }
+`;
+
+// @ts-ignore
+export const CinemaName = styled.h1`
+  color: white;
+  padding: 30px;
+`;
